ec9133e969888e76ebe87c1837960d41b93b2de8
zachard <arieszpzeng@yeah.net>
2017-06-02 21:53:25 +0800

    finish: rebase分支第一次rebase之前develop分支的提交

--- src/main/java/com/richard/java/git/rebase/RebaseFile.java
+++ src/main/java/com/richard/java/git/rebase/RebaseFile.java
@@ -71,6 +71,7 @@ public class RebaseFile {
 	public void rebaseEachPush(int times) {
 		logger.info("第二次提交");
 		logger.info("先执行合并，然后再撤销操作，执行变基");
+		logger.info("rebase分支第一次rebase之前的develop分支提交");
 	}
 
 }
