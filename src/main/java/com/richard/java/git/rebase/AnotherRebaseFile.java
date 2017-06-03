add793a7ff76c450c9279258041ec4a9c6e1ee3d
zachard <arieszpzeng@yeah.net>
2017-06-03 23:37:17 +0800

    finish: feature-rebase分支在第一次rebase之前的第二次提交

--- src/main/java/com/richard/java/git/rebase/AnotherRebaseFile.java
+++ src/main/java/com/richard/java/git/rebase/AnotherRebaseFile.java
@@ -61,6 +61,7 @@ public class AnotherRebaseFile {
 	public void methodEachRebase(int times) {
 		logger.info("第二次提交");
 		logger.info("feature-rebase分支在第一次rebase之前的第一次提交");
+		logger.info("feature-rebase分支在第一次rebase之前的第二次提交");
 	}
 
 }
